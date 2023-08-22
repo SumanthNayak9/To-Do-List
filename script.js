18c89251d581082547a1181f3be193dcff25cdca
SumanthNayak9 <98399440+SumanthNayak9@users.noreply.github.com>
2023-08-22 13:08:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 document.addEventListener("DOMContentLoaded", function() {
     const todoForm = document.getElementById("todo-form");
     const todoInput = document.getElementById("todo");
@@ -5,6 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const todoTable = document.getElementById("todo-table");
     const sortButton = document.getElementById("sort-button");
     const addedTodos =[];
+    let sortAsc =true;
 
     todoForm.addEventListener("submit", function(event) {
         event.preventDefault();
@@ -16,13 +19,14 @@ document.addEventListener("DOMContentLoaded", function() {
             newRow.innerHTML = `
                 <td>${todoText}</td>
                 <td>${todoTime}</td>
-                <td><button id="btn" class="toggle-button">Done</button></td>
-                <td><button id="btn" class="delete-button"><i class="fas fa-trash"></i></button></td>
-                <td><button id="btn" class="edit-button"><i class="fas fa-edit"></i></button></td>
+                <td><button id="btn" class="toggle-button btn btn-success" >Done</button></td>
+                <td><button id="btn" class="delete-button btn btn-outline-light" ><i class="fas fa-trash delete-button"></i></button></td>
+                <td><button id="btn" class="edit-button btn btn-outline-light" ><i class="fas fa-edit edit-button"></i></button></td>
             `;
 
             todoTable.querySelector("tbody").appendChild(newRow);
             addedTodos.push(todoText);
+            console.log(addedTodos)
             todoInput.value = "";
             todoTimein.value = "";
 
@@ -36,13 +40,31 @@ document.addEventListener("DOMContentLoaded", function() {
         const target = event.target;
 
         if (target.classList.contains("delete-button")) {
-            target.closest("tr").remove();
-        } else if (target.classList.contains("edit-button")) {
-            const todoText = target.closest("tr").querySelector("td:first-child");
-            const newText = prompt("Edit todo:", todoText.textContent);
+            const todoRow = target.closest("tr");
+            const todoText = todoRow.querySelector("td:first-child").textContent;
+            const index = addedTodos.indexOf(todoText);
+            if (index!=1){
+                addedTodos.splice(index,1);
+            }
+            todoRow.remove();
 
-            if (newText) {
-                todoText.textContent = newText;
+        }
+        else if (target.classList.contains("edit-button")) {
+            const todoRow = target.closest("tr");
+            const todoTextCell = todoRow.querySelector("td:first-child");
+            const todoText = todoTextCell.textContent;
+
+            const newText = prompt("Edit todo:", todoText);
+
+            if (newText && newText !== todoText && !addedTodos.includes(newText)) {
+                const index = addedTodos.indexOf(todoText);
+                if (index !== -1) {
+                    addedTodos[index] = newText;
+                }
+                todoTextCell.textContent = newText;
+            }
+            else if (addedTodos.includes(newText)) {
+                alert("The edited todo item already exists in the list.");
             }
         }
         else if (target.classList.contains("toggle-button")) {
@@ -52,16 +74,20 @@ document.addEventListener("DOMContentLoaded", function() {
             todoRow.classList.toggle("done");
             if (todoRow.classList.contains("done")) {
                 todoText.classList.add("done");
+                target.classList.remove("btn-success");
+                target.classList.add("btn-danger");
                 target.textContent = "Undone";
             } else {
                 todoText.classList.remove("done");
+                target.classList.remove("btn-danger");
+                target.classList.add("btn-success");
                 target.textContent = "Done";
             }
         }
     });
 
  
-    sortButton.addEventListener("click", function()) {
+    sortButton.addEventListener("click", function() {
         const rows = Array.from(todoTable.querySelectorAll("tbody tr"));
         const doneRows = rows.filter(row => row.classList.contains("done"));
         const undoneRows = rows.filter(row => !row.classList.contains("done"));
@@ -69,10 +95,21 @@ document.addEventListener("DOMContentLoaded", function() {
         // Clear the table
         todoTable.querySelector("tbody").innerHTML="";
 
+        if (sortAsc === true){
+
         // Append sorted rows back to the table
-        undoneRows.forEach(row => todoTable.querySelector("tbody").appendChild(row));
-        doneRows.forEach(row => todoTable.querySelector("tbody").appendChild(row));
-    };
+            undoneRows.forEach(row => todoTable.querySelector("tbody").appendChild(row));
+            doneRows.forEach(row => todoTable.querySelector("tbody").appendChild(row));
+            sortAsc=false;
+        }
+        else{
+            doneRows.forEach(row => todoTable.querySelector("tbody").appendChild(row));
+            undoneRows.forEach(row => todoTable.querySelector("tbody").appendChild(row));
+            sortAsc=true;
+
+        }
+
+    });
 
 
 });
